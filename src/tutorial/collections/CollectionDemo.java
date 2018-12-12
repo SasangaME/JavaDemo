6071113e40233adcf6cb52afb7f2c1249ec91ec9
Sasanga Madhumal Edirisinghe <smadhumal@hotmail.com>
2018-12-13 00:09:10 +0530

    day end commit
    Signed-off-by: Sasanga Madhumal Edirisinghe <smadhumal@hotmail.com>

--- src/tutorial/collections/CollectionDemo.java
+++ src/tutorial/collections/CollectionDemo.java
@@ -9,7 +9,11 @@ public class CollectionDemo
 //        arrayListDemo();
 //        Container<Integer> obj = new Container<>();
 //        obj.show();
-          listDemo();
+//          listDemo();
+//            vectorDemo();
+//        comparatorDemo();
+//        comparetorDemo();
+        setDemo();
 
     }
 
@@ -51,8 +55,78 @@ public class CollectionDemo
         lst.forEach(System.out::println);
     }
 
+    public static void vectorDemo()
+    {
+        Vector v = new Vector();
+        v.add(4);
+        v.add(6);
 
+        System.out.println(v.capacity());
+    }
 
+    public static void comparatorDemo()
+    {
+        List<Integer> lst = new ArrayList<>();
+        lst.add(245);
+        lst.add(246);
+        lst.add(444);
+        lst.add(111);
+        lst.add(99);
+        lst.add(999);
+
+        Comparator<Integer> com = (o1, o2) -> {
+          return (o1 > o2) ? 1 : -1;
+        };
+
+        Collections.sort(lst, com);
+
+        for(int i: lst)
+            System.out.println(i);
+    }
+
+    public static void comparetorDemo()
+    {
+        Test t1 = new Test();
+        t1.id = 1;
+        t1.val = 200;
+
+        Test t2 = new Test();
+        t2.id = 2;
+        t2.val = 100;
+
+        Test t3 = new Test();
+        t3.id = 3;
+        t3.val = 300;
+
+        List<Test> lst = new ArrayList<>();
+        lst.add(t1);
+        lst.add(t2);
+        lst.add(t3);
+
+        Comparator<Test> com = (l1, l2) -> {
+            return (l1.val > l2.val) ? 1 : -2;
+        };
+
+        Collections.sort(lst, com);
+
+        for(Test t : lst)
+            System.out.println(t.id + " - " + t.val);
+    }
+
+    public static void setDemo()
+    {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(5);
+        set.add(6);
+        set.add(7);
+        set.add(6);
+        set.add(8);
+        set.add(5);
+
+        for(int i : set)
+            System.out.println(i);
+    }
 
 }
 
@@ -77,4 +151,21 @@ class Container<T>
     {
 
     }
+}
+
+class ComImpl implements  Comparator<Integer>
+{
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if(o1 > o2)
+            return 1;
+        return -1;
+    }
+}
+
+class Test
+{
+    int id;
+    int val;
 }
