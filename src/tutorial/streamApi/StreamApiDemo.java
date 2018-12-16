86acf87a96ba85977b7975534761af388d4d9848
Sasanga Madhumal Edirisinghe <smadhumal@hotmail.com>
2018-12-16 23:02:35 +0530

    stream reduce predicate
    Signed-off-by: Sasanga Madhumal Edirisinghe <smadhumal@hotmail.com>

--- src/tutorial/streamApi/StreamApiDemo.java
+++ src/tutorial/streamApi/StreamApiDemo.java
@@ -3,6 +3,7 @@ package tutorial.streamApi;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamApiDemo {
 
@@ -10,7 +11,7 @@ public class StreamApiDemo {
     {
         try
         {
-          mapDemo();
+          reducePredicateDemo();
         }
         catch (Exception e)
         {
@@ -52,5 +53,14 @@ public class StreamApiDemo {
     {
         List<Integer> lst = Arrays.asList(1,2,3,4,5,6);
         System.out.println(lst.stream().map(i -> i*2).reduce(0, (c, e) -> Integer.sum(c,e)));
+    }
+
+    public static  void reducePredicateDemo()
+    {
+        List<Integer> lst = Arrays.asList(15, 20 , 25, 27, 30, 32);
+        List<Integer> lst2 =  lst.stream().filter(i -> i % 5 == 0).collect(Collectors.toList());
+        lst2.forEach(i -> System.out.println(i));
+        int sum = lst2.stream().reduce(0, (c, e) -> Integer.sum(c, e));
+        System.out.println("Sum of new list = " + sum);
     }
 }
