dbcdad16f6d6a8ac84ef16953a6fa2c6fb55d99a
Sasanga Madhumal Edirisinghe <smadhumal@hotmail.com>
2018-12-16 12:33:57 +0530

    jdbc insert
    Signed-off-by: Sasanga Madhumal Edirisinghe <smadhumal@hotmail.com>

--- src/tutorial/jdbc/JdbcDemo.java
+++ src/tutorial/jdbc/JdbcDemo.java
@@ -3,6 +3,7 @@ package tutorial.jdbc;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class JdbcDemo {
@@ -12,6 +13,7 @@ public class JdbcDemo {
         {
 //           getSingleData();
             JdbcDemo obj = new JdbcDemo();
+            obj.createRecord();
             List<Student> lst = obj.getAllData();
             for(Student s : lst)
             {
@@ -66,6 +68,37 @@ public class JdbcDemo {
         }
 
         return lst;
+    }
+
+    public void createRecord() throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+
+
+        String url = "jdbc:mysql://localhost:3306/jdbcdemodb";
+        String username = "root";
+        String password = "sasanga";
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        Statement st = con.createStatement();
+        String qry1 = "SELECT MAX(id) as id FROM student";
+        ResultSet rs1 = st.executeQuery(qry1);
+
+        rs1.next();
+        int id = rs1.getInt("id") + 1;
+        st.close();
+
+
+        String qry2 = "INSERT INTO student(Id, Name) VALUES(?, ?)";
+        PreparedStatement st2 = con.prepareStatement(qry2);
+        st2.setInt(1, id);
+        st2.setString(2, name);
+        st2.executeUpdate();
+
+        st2.close();
+        con.close();
     }
 }
 
